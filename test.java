37d88eacb7d5bbd62d59a4e669860acfd3197e84
Saurabhkadam123 <101618292+Saurabhkadam123@users.noreply.github.com>
2022-03-15 12:08:47 +0530

    java main function test files

---
+++ test.java
@@ -0,0 +1,17 @@
+class Test
+{
+public static void main(String[] args)
+{System.out.print("hello\n");
+System.out.print("hello\n");
+System.out.print("hello\n");
+System.out.print("hello\n");
+System.out.print("hello\n");
+System.out.print("hello\n");
+System.out.print("hello\n");
+System.out.print("hello\n");
+System.out.print("hello\n");
+System.out.print("hello0");
+System.out.print("hello\n");
+System.out.println("hello");
+}
+}
